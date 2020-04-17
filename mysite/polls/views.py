43d7fd1a0285652a3682d2496da81154bed716a1
djangojeng-e <headfat1218@gmail.com>
2020-04-18 08:26:56 +0900

    Part4. 최소한의 폼 작성하기

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.template import loader
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
@@ -18,11 +18,25 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 투표 폼을 다시 출력
+        return render(request, 'polls/detail.html',
+                  {'question': question,
+                   'error_message': "You didn't select a choice",
+                    })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # 항상 POST 데이터를 성공적으로 다루었을때는
+        # HttpResponseRedirect 를 반환해 줍니다
+        # 이것으로 사용자가 데이터를 뒤로가기 버튼을 눌러서 데이터를 두번 전송하는것을 방지합니다.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
