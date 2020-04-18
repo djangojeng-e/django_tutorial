e8d889d55f72127140fe36b826106a1f11a35a11
djangojeng-e <headfat1218@gmail.com>
2020-04-18 11:31:23 +0900

    첫번째 테스트 작성하기.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+
+from .models import Question
 
 # Create your tests here.
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() 가 pub_date 가 미래 날짜로 지정되어 있는
+        질문들에 대해서 False 를 반환함
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
