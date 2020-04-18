31a648a80c6c44adbb747c12d99d352c2f6b11cf
djangojeng-e <headfat1218@gmail.com>
2020-04-18 21:49:54 +0900

    Admin form 고치기 - fiedsets 사용하기.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
-
 from .models import Question
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        ('질문내용',           {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 
 
 admin.site.register(Question, QuestionAdmin)
