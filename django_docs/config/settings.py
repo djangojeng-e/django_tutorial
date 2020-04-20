d710fe565ca146abbecc2f86c866b6349655bbae
djangojeng-e <headfat1218@gmail.com>
2020-04-20 20:06:35 +0900

    Models - Relationships (Many-to-Many - 2편)

--- django_docs/config/settings.py
+++ django_docs/config/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'usingmodel',
+    # 'usingmodel',
     'onetomany',
+    'manytomany',
     'django_extensions',
 ]
 
