a5357773b5b7f26ece6735651a7483f6e45188ac
djangojeng-e <headfat1218@gmail.com>
2020-04-22 19:50:19 +0900

    Models - Model inheritance (Meta inheritance)

--- django_docs/abstractbaseclasses/models.py
+++ django_docs/abstractbaseclasses/models.py
@@ -9,7 +9,12 @@ class CommonInfo(models.Model):
 
     class Meta:
         abstract = True
+        ordering = ['name']
 
 
 class Student(CommonInfo):
     home_group = models.CharField(max_length=5)
+
+    class Meta(CommonInfo.Meta):
+        db_table = 'student_info'
+
