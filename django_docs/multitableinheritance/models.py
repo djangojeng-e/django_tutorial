98653d00a00fa08f216f0de42c072c5a97b74f73
djangojeng-e <headfat1218@gmail.com>
2020-04-23 17:25:04 +0900

    Models Model inheritance(Multitable inheritance)2

--- django_docs/multitableinheritance/models.py
+++ django_docs/multitableinheritance/models.py
@@ -12,3 +12,9 @@ class Restaurant(Place):
     serves_hot_dogs = models.BooleanField(default=False)
     serves_pizza = models.BooleanField(default=False)
 
+
+# 새로운 서브 클래스 추가
+class Supplier(Place):
+    customers = models.ManyToManyField(Place, related_name='provider')
+
+
