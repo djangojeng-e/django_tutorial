966e5f344db6b40030d51d140a6d2e6f0599bb57
djangojeng-e <headfat1218@gmail.com>
2020-05-05 23:52:10 +0900

    Forms - Form class 뷰 생성

--- django_docs/forms_more_on_fields/views.py
+++ django_docs/forms_more_on_fields/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.core.mail import send_mail
+from .forms import ContactForm
+from django.forms import forms
+
 
 # Create your views here.
+
+
+if form.is_valid():
+    subject = form.cleaned_data['subject']
+    message = form.cleaned_data['message']
+    sender = form.cleaned_data['sender']
+    cc_myself = forms.cleaned_data['cc_myself']
+
+    recipients = ['info@example.com']
+
+    if cc_myself:
+        recipients.append(sender)
+
+    send_mail(subject, message, sender, recipients)
+    return HttpResponseRedirect('/thanks/')
