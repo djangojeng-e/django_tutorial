5244e5f9d889ffa2cd74fae916207e96dd507b11
djangojeng-e <headfat1218@gmail.com>
2020-05-14 16:02:36 +0900

    generic views - get_context_data in DetailView

--- django_docs/generic_views/views.py
+++ django_docs/generic_views/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Publisher
+from django.views.generic import ListView, DetailView
+from .models import Publisher, Book
 
 
 class PublisherList(ListView):
     model = Publisher
     context_object_name = 'my_favourite_publishers'
+
+
+class PublisherDetail(DetailView):
+    model = Publisher
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['book_list'] = Book.objects.all()
+        return context
 
 
 
