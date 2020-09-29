1f10554cb720f68d6362f8d4961eac7242009234
djangojeng-e <headfat1218@gmail.com>
2020-09-29 19:43:07 +0900

    Many-To-Many Practice followed documentation

--- django_docs/manytomany/models.py
+++ django_docs/manytomany/models.py
@@ -39,3 +39,25 @@ class Machine(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    publications = models.ManyToManyField(Publication)
+
+    class Meta:
+        ordering = ['headline']
+
+    def __str__(self):
+        return self.headline
+
